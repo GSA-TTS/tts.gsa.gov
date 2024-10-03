4467d6b70d1c64a7f6841896ac5be406063860ad
Wes Dean <wesley.dean@gsa.gov>
2024-10-03 15:40:39 -0400

    Add script to actually do the yamllinting

--- bin/yaml_lint_front_matter.bash
+++ bin/yaml_lint_front_matter.bash
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
-## @file extract_front_matter.bash
+## @file yaml_lint_front_matter.bash
 ## @author TTS Website Team
-## @brief extracts YAML front matter from Markdown
+## @brief runs YAML linter on Markdown front matter
 ## @details
+## While not officially a commonly-recognized standard (i.e., it's not
+## included in CommonMark or GFM specifications), some tools (e.g.,
+## Static Site Generators (SSGs) like Jekyll and Eleventy) allow for
+## the inclusion of YAML data at the start of Markdown file.  This
+## is often used to specify metadata such as page name, permalink,
+## etc..
+##
+## Front matter is typically (although not necessarily) YAML; it's
+## separated from the Markdown content by a `---` at the start
+## and end of the front matter block.
+##
 ## The situation we encountered was that we had Markdown files that included
 ## YAML front matter that was improperly formatted.  The Markdown linter we
 ## were using ignored YAML front matter in Markdown while the YAML linter
 ## wasn't processing Markdown files (and if it did, the Markdown outside of
 ## the YAML front matter would cause it to fail.
 ##
-## This Bash script goes line by line through a Markdown file and extracts
-## the YAML front matter:
-##
-## Up to the first `---`, write a `#` (a comment in YAML)
-## After the second `---`, do nothing
-## After the first and before the second `---`, write the line
+## This Bash script goes line by line through Markdown files and extracts
+## the YAML front matter, and then runs the YAML through yamllint.
 ##
 ## Consider the following:
 ##
@@ -41,17 +48,13 @@
 ## The front matter could then be processed by a YAML linter without any
 ## Markdown gumming up the works.
 ##
-## @par Examples
-## @code
-## for file in *.md ; do
-##   if [ -e "$file" ] ; then
-##     tempfile="${file}.yaml"
-##     extact_front_matter.bash < "$file" > $"tempfile"
-##     yamllint "$tempfile" | sed -Ee "s|$tempfile|$file|g"
-##     rm -f "$tempfile"
-##  fi
-## done
-## @endcode
+## This tool will iterate across zero or more filenames -- typically those
+## corresponding to Markdown content (i.e., '.md' files), extract the
+## YAML from them, run the YAML through yamllint, and return any warnings
+## via STDOUT.  The tool will return the net result code from running yamllint
+## across all files (e.g., if all files pass (return 0), the tool will
+## return 0; if yamllint were to return a 1 on one file and 0 for all
+## of the others, the tool will return 1.
 
 set -euo pipefail
 
@@ -88,7 +91,24 @@ extract_front_matter() {
 ## @fn main()
 ## @brief the main function
 main() {
-  extract_front_matter
+
+  local clean=0
+
+  for filename in "$@" ; do
+    temp_filename="$(mktemp)"
+
+    extract_front_matter < "$filename" > "$temp_filename"
+
+    if ! results="$(yamllint --strict --format standard "$temp_filename")" ; then
+      clean=$?
+    fi
+
+    echo -n "$results" | sed -Ee "s|^$temp_filename\b|$filename|g"
+
+    rm -f "$temp_filename"
+  done
+
+  return $clean
 }
 
 # if we're not being sourced and there's a function named `main`, run it
