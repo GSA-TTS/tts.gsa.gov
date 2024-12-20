1d3933cf29243ee21f17596bc6623454c666e339
Wesley Dean <wesley.dean@gsa.gov>
2024-12-20 15:45:29 +0000

    [MegaLinter] Apply linters fixes

--- js/redirect.js
+++ js/redirect.js
@@ -1,11 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const archiveDir = './pages/jointts/positions/archive';
+const archiveDir = "./pages/jointts/positions/archive";
 
-const mdFiles = fs.readdirSync(archiveDir).filter(file => file.endsWith('.md'));
+const mdFiles = fs
+  .readdirSync(archiveDir)
+  .filter((file) => file.endsWith(".md"));
 
-module.exports = mdFiles.map(file => ({
-  permalink: `/pages/jointts/positions/archive/${file.replace('.md', '.html')}`,
-  redirectTo: '/join/',
+module.exports = mdFiles.map((file) => ({
+  permalink: `/pages/jointts/positions/archive/${file.replace(".md", ".html")}`,
+  redirectTo: "/join/",
 }));
